8f1a94cf6b2a9d03358df1d4e7f87b60820ad34f
13256908633 <870962931@qq.com>
2018-08-30 23:43:15 +0800

    dao实现

---
+++ src/main/java/dao/CommentInfoDaoImpl.java
@@ -0,0 +1,24 @@
+package dao;
+
+import entity.Comment;
+
+import java.util.List;
+
+public class CommentInfoDaoImpl extends BaseDao<Comment> implements CommentInfoDao {
+
+    @Override
+    public int insertComment(Comment comment) {
+
+        return executeUpdate("insert into t_comment (commentId,blogId,userId,comDate,comContent,num) values(?,?,?,?,?,?)",new Object[]{comment.getCommentId(),comment.getBlogId(),comment.getUserId(),comment.getComDate(),comment.getComContent(),comment.getNum()});
+    }
+
+    @Override
+    public int deleteComment(int commentId) {
+        return executeUpdate("delete from t_comment where commentId=?",new Object[]{commentId});
+    }
+
+    @Override
+    public List<Comment> getAllComment(int blogId) {
+        return executeQuery("select * from t_comment");
+    }
+}
---
+++ src/main/java/dao/InformDaoImpl.java
@@ -0,0 +1,23 @@
+package dao;
+
+import entity.Inform;
+
+import java.util.List;
+
+public class InformDaoImpl extends BaseDao<Inform> implements InformDao {
+    @Override
+    public int insertInform(Inform inform) {
+        return executeUpdate("insert into t_inform(informId ,userId, informContent, warnDate, informStatus,reporterId)values(?,?,?,?,?,?)",new Object[]{inform.getInformId(),inform.getUserId(),inform.getInformContent(),inform.getWarnDate(),inform.getInformStatus(),inform.getReporterId()});
+
+    }
+
+    @Override
+    public List<Inform> getInformById(int informId) {
+        return executeQuery("select * from t_inform where informId=?",new Object[]{informId});
+    }
+
+    @Override
+    public List<Inform> getAllInform() {
+        return executeQuery("select * from t_inform");
+    }
+}
