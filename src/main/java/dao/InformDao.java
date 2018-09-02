74da8f2fc91744f3a345fc54475dd623cfbc3e5b
lmmzy <1456621978@qq.com>
2018-09-03 00:25:25 +0800

    点赞、评论、转发、关注 的dao 都有微小改动

--- src/main/java/dao/InformDao.java
+++ src/main/java/dao/InformDao.java
@@ -8,7 +8,7 @@ public interface InformDao {
     //增加通知
     int insertInform(Inform inform);
     //查看通知
-    List<Inform> getInformById(int informId);
+    List<Inform> getInformById(int userId);
     //查看所有通知
     List<Inform> getAllInform();
 }
